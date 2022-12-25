529b07b02fec8c79301b646a18d264427c8adbc0
Rami Hikmat <ramihhikmat@outlook.com>
2022-12-25 17:41:49 +1100

    Use Mantine & Description (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Container, Flex, MantineProvider } from '@mantine/core';
 import React from 'react';
+import TrainSystemVisualiser from './components/train-system-visualiser/train-system-visualiser';
 
 function App() {
-	return <div>Hello world</div>;
+	return (
+		<MantineProvider>
+			<TrainSystemVisualiser />
+		</MantineProvider>
+	);
 }
 
 export default App;
